feae4c671f9d959b618366b9419bb858805dcbeb
Gwilherm Baudic <gbaudic@users.noreply.github.com>
2019-01-23 09:46:30 +0100

    Adding input box to main library header

--- include/guisan.hpp
+++ include/guisan.hpp
@@ -97,6 +97,7 @@
 #include <guisan/widgets/icon.hpp>
 #include <guisan/widgets/imagebutton.hpp>
 #include <guisan/widgets/imagetextbutton.hpp>
+#include <guisan/widgets/inputbox.hpp>
 #include <guisan/widgets/label.hpp>
 #include <guisan/widgets/listbox.hpp>
 #include <guisan/widgets/messagebox.hpp>
